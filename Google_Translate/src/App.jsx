053dd165e0e516343e163c0eb27b175d7201a9ec
filipesilva9225 <filipesilva9225@gmail.com>
2025-08-25 17:46:12 -0300

    add all files

---
+++ Google_Translate/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+          <h1 className="">Google Translate</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
